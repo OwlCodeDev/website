9ce579a5e2511ba6cee935e13073b985c6b3625f
José Gabriel Gruber <dev@jgabrielgruber.dev>
2023-10-24 08:45:09 +0000

    Improved content and layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <ThemeProvider>
-        <div className="navbar sticky top-0 z-30">
+        <div className="navbar sticky top-0 z-30 border-b border-slate-900/10 lg:border-0 dark:border-slate-300/10">
           <div className="flex-1">
             <a className="btn btn-ghost normal-case text-xl">OwlCode</a>
           </div>
@@ -22,22 +22,22 @@ function App() {
             className="hero min-h-screen"
             style={{backgroundImage: 'url(https://images.pexels.com/photos/3861967/pexels-photo-3861967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'}}
           >
-            <div className="hero-overlay bg-opacity-80"></div>
+            <div className="hero-overlay bg-opacity-80 backdrop-blur-sm"></div>
             <div className="hero-content text-neutral-content">
               <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold" id="intro">Hello there</h1>
+                <h1 className="mb-5 text-5xl font-bold" id="intro">Hello there!</h1>
                 <p className="mb-5">
-                  At OwlCode, we are driven by our passion for innovation and quality. 
+                  This is OwlCode, we are driven by our passion for innovation and quality. 
                   Our software solutions are crafted with meticulous attention to detail, 
                   and we approach every project with a relentless pursuit of excellence..
                 </p>
               </div>
             </div>
           </div>
-          <div className="hero min-h-screen bg-secondary">
+          <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
               <img src="https://images.pexels.com/photos/12899189/pexels-photo-12899189.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="max-w-sm rounded-lg shadow-2xl" />
-              <div>
+              <div className="max-w-lg">
                 <h1 className="text-5xl font-bold" id="about">Who we are?</h1>
                 <ul className="py-6 list-disc list-inside">
                   <li>A team of passionate professionals united by a shared vision of excellence</li>
@@ -48,10 +48,10 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="hero min-h-screen bg-primary">
+          <div className="hero min-h-screen bg-base-100">
             <div className="hero-content flex-col lg:flex-row">
               <img src="https://images.pexels.com/photos/18152964/pexels-photo-18152964/free-photo-of-woman-in-a-futuristic-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="max-w-sm rounded-lg shadow-2xl" />
-              <div>
+              <div className="max-w-lg">
                 <h1 className="text-5xl font-bold" id="stack">Our Technical Stack:</h1>
                 <ul className="py-6 list-disc list-inside">
                   <li>Frontend: ReactJS, TailwindCSS</li>
@@ -67,7 +67,7 @@ function App() {
 
         <footer className="footer footer-center p-4 bg-base-300 text-base-content">
           <aside>
-            <p>Copyright © 2023 - All right reserved by OwlCode Ltd</p>
+            <p>Copyright © 2023 - All right reserved by <a href="https://owlcode.dev">OwlCode</a> Ltd</p>
           </aside>
         </footer>
       </ThemeProvider>
