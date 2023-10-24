9c0a89849aea08d6b30804cf14d59c8800d96304
José Gabriel Gruber <dev@jgabrielgruber.dev>
2023-10-24 10:10:20 +0000

    Fixed nav

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,22 @@ function App() {
           </div>
           <div className="flex-none">
             <ul className="menu menu-horizontal px-1">
-              <li><a>Intro</a></li>
-              <li><a>About us</a></li>
-              <li><a>Tech stack</a></li>
+              <li><a href="#intro">Intro</a></li>
+              <li><a href="#about">About us</a></li>
+              <li><a href="#stack">Tech stack</a></li>
             </ul>
           </div>
         </div>
 
         <>
           <div
-            className="hero min-h-screen"
+            className="hero min-h-screen" id="intro"
             style={{backgroundImage: 'url(https://images.pexels.com/photos/3861967/pexels-photo-3861967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'}}
           >
             <div className="hero-overlay bg-opacity-80 backdrop-blur-sm"></div>
             <div className="hero-content text-neutral-content">
               <div className="max-w-lg">
-                <h1 className="font-title text-center text-[clamp(2rem,6vw,4.2rem)] font-black leading-[1.1] xl:text-left" id="intro">
+                <h1 className="font-title text-center text-[clamp(2rem,6vw,4.2rem)] font-black leading-[1.1] xl:text-left">
                   <span className="[&::selection]:text-base-content brightness-150 contrast-150 [&::selection]:bg-blue-700/20">Igniting</span>
                   <span className="inline-grid">
                     <span className="pointer-events-none col-start-1 row-start-1 bg-[linear-gradient(90deg,hsl(var(--s))_0%,hsl(var(--sf))_9%,hsl(var(--pf))_42%,hsl(var(--p))_47%,hsl(var(--a))_100%)] bg-clip-text opacity-70 blur-3xl [transform:translate3d(0,0,0)] [-webkit-text-fill-color:transparent] [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,hsl(var(--s))_4%,color-mix(in_oklch,hsl(var(--sf)),hsl(var(--pf)))_22%,hsl(var(--p))_45%,color-mix(in_oklch,hsl(var(--p)),hsl(var(--a)))_67%,hsl(var(--a))_100.2%)]" aria-hidden="true">the future</span>
@@ -42,7 +42,7 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="hero min-h-screen bg-base-200 bg-contain bg-repeat"
+          <div className="hero min-h-screen bg-base-200 bg-contain bg-repeat" id="about"
               style={{
                 backgroundImage: 'url(https://images.pexels.com/photos/12899189/pexels-photo-12899189.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
                 backgroundPosition: '80% 50%',
@@ -54,7 +54,7 @@ function App() {
               <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src="https://images.pexels.com/photos/12899189/pexels-photo-12899189.jpeg?auto=compress&cs=tinysrgb&w=1080&h=1920&dpr=1" className="max-w-sm rounded-lg shadow-2xl" />
                 <div className="max-w-lg">
-                  <h1 className="text-5xl font-bold" id="about">Who we are?</h1>
+                  <h1 className="text-5xl font-bold">Who we are?</h1>
                   <ul className="py-6 list-disc list-inside">
                     <li>A team of passionate professionals united by a shared vision of excellence</li>
                     <li>Committed to delivering innovative, high-quality software solutions that exceed expectations</li>
@@ -65,7 +65,7 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="hero min-h-screen bg-base-100 bg-contain bg-repeat"
+          <div className="hero min-h-screen bg-base-100 bg-contain bg-repeat" id="stack"
               style={{
                 backgroundImage: 'url(https://images.pexels.com/photos/18152964/pexels-photo-18152964/free-photo-of-woman-in-a-futuristic-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
                 backgroundPosition: '20% 50%',
@@ -77,7 +77,7 @@ function App() {
               <div className="hero-content flex-col lg:flex-row">
                 <img src="https://images.pexels.com/photos/18152964/pexels-photo-18152964/free-photo-of-woman-in-a-futuristic-room.jpeg?auto=compress&cs=tinysrgb&w=1080&h=1920&dpr=1" className="max-w-sm rounded-lg shadow-2xl" />
                 <div className="max-w-lg">
-                  <h1 className="text-5xl font-bold" id="stack">Our Technical Stack:</h1>
+                  <h1 className="text-5xl font-bold">Our Technical Stack:</h1>
                   <ul className="py-6 list-disc list-inside">
                     <li>Frontend: ReactJS, TailwindCSS</li>
                     <li>Backend: NodeJS and Python</li>
