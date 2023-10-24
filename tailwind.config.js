ef35a799d0b532249d0cc653211a7d448f553434
José Gabriel Gruber <dev@jgabrielgruber.dev>
2023-10-24 21:39:18 +0000

    Stylized text

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      code: ["Source\ Code\ Pro", "monospace"],
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
