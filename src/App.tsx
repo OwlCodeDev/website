e36c5e0c6200d57b6c95cd82219b368fd6c52eb7
drosdek <josnei.drosdek@gmail.com>
2023-10-25 21:56:49 -0300

    Implementado: Hero sobre nos;

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { ThemeProvider } from "./contexts/theme/theme.context";
 
 import NavBarComponent from "./components/NavBar/navBar.component";
 import InitialHero from "./components/Hero/InitialHero";
+import AboutUsHero from "./components/Hero/AboutUs";
+import ContactComponent from "./components/Contact";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <ThemeProvider>
         <NavBarComponent />
         <InitialHero />
+        <AboutUsHero />
+        <ContactComponent />
       </ThemeProvider>
     </>
   );
